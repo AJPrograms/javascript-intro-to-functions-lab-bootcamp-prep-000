f7957f140f3915088592bf67b7fad2f4ebcb2372
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 04:32:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,15 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   if (string === string.toLowerCase())
   return 'I can\'t hear you!'
   else if (string === string.toUpperCase())
   return 'YES INDEED!'
   else if (string === 'I love you, Grandma.')
   return('I love you, too.');
+=======
+  if sayHiToGrandma('hello')
+  return 
+>>>>>>> 104ad2c6638507f8ad4fcf1c8c57460b47e82616
 }
